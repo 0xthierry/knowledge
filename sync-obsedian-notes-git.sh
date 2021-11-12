eece37bc3daca6fa38955db6d06b879af39716d5
Thierry Santos <thierrysantoos123@gmail.com>
2021-11-12 07:56:24 -0300

    syncing obesidian notes 11/12/21 07:56:24

---
+++ sync-obsedian-notes-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash          
+obisidian_folder="/Users/thierrysantos/Documents/brain/brain"
+destination_folder=$(pwd)
+current_time=$(date +"%D %T")
+
+cd $obisidian_folder
+cp -R . $destination_folder
+cd $destination_folder
+rm -rf .obsidian
+git add .
+git commit -m "syncing obesidian notes $current_time"
